77f2c290dbd3101b11404394ace394ffae2bcb68
Arun Kumar <aruncode49@gmail.com>
2024-01-03 17:27:47 +0530

    complete html and javascript code

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let currOptions = document.querySelectorAll("form select");
+let convertBtn = document.querySelector("form button");
+let amount = document.querySelector("form input");
+let fromCurr = document.querySelector("form .from-currency");
+let toCurr = document.querySelector("form .to-currency");
+let resultText = document.querySelector("form .result");
+let exchangeIcon = document.querySelector("#exchange-icon");
+const apiURL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
 // fetching currency codes
 function fetchAllCurrencyCodes(element) {
@@ -16,6 +24,19 @@ async function updateFlag(imgElement, countryCode) {
   imgElement.src = updatedSrc;
 }
 
+// convert currency
+async function convertCurrency() {
+  if (amount.value === "" || amount.value < 1) amount.value = 1;
+  const res = await fetch(
+    `${apiURL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`
+  );
+  const data = await res.json();
+  const exchangeCurr = toCurr.value.toLowerCase();
+  const exchangeRate = data[exchangeCurr];
+  const conversion = (amount.value * exchangeRate).toFixed(2);
+  resultText.innerHTML = `${amount.value} ${fromCurr.value} = ${conversion} ${toCurr.value}`;
+}
+
 currOptions.forEach((select) => {
   fetchAllCurrencyCodes(select); // fetch and append options
   select.addEventListener("change", (e) => {
@@ -24,3 +45,20 @@ currOptions.forEach((select) => {
     updateFlag(selectedOptionImageEl, countryList[selectedOption.value]);
   });
 });
+
+exchangeIcon.addEventListener("click", (e) => {
+  const temp = fromCurr.value;
+  fromCurr.value = toCurr.value;
+  toCurr.value = temp;
+  const fromCountry = countryList[fromCurr.value];
+  const toCountry = countryList[toCurr.value];
+  fromCurr.previousElementSibling.src = `https://flagsapi.com/${fromCountry}/flat/64.png`;
+  toCurr.previousElementSibling.src = `https://flagsapi.com/${toCountry}/flat/64.png`;
+});
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convertCurrency();
+});
+
+window.addEventListener("load", convertCurrency);
